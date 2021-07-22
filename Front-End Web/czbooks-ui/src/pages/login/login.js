833f724c05fd76846e0da8e28effffddf64bcc14
Kemilly Ferreira <kemilly.sf@gmail.com>
2021-07-22 09:32:19 -0300

    continuação do front-end

--- Front-End Web/czbooks-ui/src/pages/login/login.js
+++ Front-End Web/czbooks-ui/src/pages/login/login.js
@@ -15,32 +15,38 @@ class Login extends Component
         };
     };
 
+    // função que faz a chamada para a API para realizar o login
     efetuaLogin = (event) => 
     {
+        //prevenir um comportamento padrão, por exemplo recarregar a página 
         event.preventDefault();
 
         axios.post('http://localhost:5000/api/login',
         {
-            //email tem que ser igual state.email, o mesmo vale para senha
+            // email tem que ser igual state.email, o mesmo vale para senha
             email : this.state.email,
             senha : this.state.senha
         })
 
-        // se não for um, tenta o outro, não tem como ser os dois ao mesmo tempo
+        // verifica o retorno da requisição; se não for um, tenta o outro, não tem como ser os dois ao mesmo tempo
         .then(resposta =>
             {
+                // caso o status code seja 200
                 if(resposta.status == 200)
-
-                //salva o token na base
+                // salva o token no localStorage
                 localStorage.setItem('login-usuario', resposta.data.token)
+                // exibe o token no console do navegador
                 console.log('Meu token é: '+ resposta.data.token)
 
+                //verifica se o tipo de usuário logado é adm
+                //caso seja, vai redirecionar para a página de atendimentos
                 if(parseJwt().role == "1")
                 {
                     console.log(this.props)
                     this.props.history.push('/atendimentos')
                 }
 
+                // se não for, redireciona para a página home
                 else
                 {
                     this.props.history.push('/')
@@ -49,9 +55,41 @@ class Login extends Component
             .catch(erro => console.log(erro));
     };
 
+    // função genérica que atualiza o state de acordo com o input
     atualizaStateCampo = (campo) =>
     {
         this.setState({ [campo.target.name] : campo.target.value })
+    };
+
+    render()
+    {
+        return(
+        <div>
+            <h1>Login</h1>
+
+            <section> 
+                <form onSubmit={this.efetuaLogin}>
+                    
+                    <input
+                        type="text"
+                        name="email"
+                        value={this.state.email}
+                        onChange={this.atualizaStateCampo}
+                        placeholder="username"
+                        />
+
+                    <input
+                    type="password"
+                    name="senha"
+                    value={this.state.senha}
+                    onChange={this.atualizaStateCampo}
+                   placeholder={"password"}
+                   />
+                </form>
+
+            </section>
+        </div>
+        )
     }
    
 }
--- Front-End Web/czbooks-ui/src/services/auth.js
+++ Front-End Web/czbooks-ui/src/services/auth.js
@@ -1,8 +1,13 @@
+// constante para ver se o usuário está loagdo
 export const usuarioautenticado = () => localStorage.getItem('login-usuario') !== null;
 
+// constante que retorna o payload do usuario covnvertido em JSON
 export const parseJwt = () =>
 {
+    // define a variável que recebe o payload do usuario logado codificado
     let base64 =localStorage.getItem('login-usuario').split('.')[1];
 
+    // decodifica a base64 para string
+    // e converte a string para JSON
     return JSON.parse(window.atob(base64));
 };
